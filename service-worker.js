223233e2bac8a5078c2bd2541ba9e5186084e32e
Regalx1 <118701083+Regalx1@users.noreply.github.com>
2025-12-17 19:30:33 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// Simple service worker for Number Arena 3D PWA
+const CACHE_NAME = 'number-arena-3d-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './main.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Only handle GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
